bff5b63ab07fcebac034a1f629a91fc8d314cc2c
Ray Krueger <raykrueger@gmail.com>
2022-06-14 18:25:05 +0000

    Add experimental support, see STEAMARGS

--- Makefile
+++ Makefile
@@ -6,5 +6,8 @@ build:
 run: build
 	docker run --rm -it -p 7777:7777/udp -p 15777:15777/udp -p 15000:15000/udp raykrueger/satisfactory
 
+run-experimental: build
+	docker run --rm -it -e STEAMARGS="-beta experimental" -p 7777:7777/udp -p 15777:15777/udp -p 15000:15000/udp raykrueger/satisfactory
+
 shell: build
 	docker run --rm -it --entrypoint /bin/bash raykrueger/satisfactory
--- start-server.sh
+++ start-server.sh
@@ -7,6 +7,6 @@ gomplate --input-dir /tmp/config --output-dir /game/FactoryGame/Saved/Config/Lin
 chown -R ${USERID}:${USERID} /game/FactoryGame/Saved
 
 echo "Updating game server"
-gosu $USERID:$USERID steamcmd +force_install_dir /game +login anonymous +app_update 1690800 +quit
+gosu $USERID:$USERID steamcmd +force_install_dir /game +login anonymous +app_update 1690800 $STEAMARGS +quit
 
 gosu $USERID:$USERID "./Engine/Binaries/Linux/UE4Server-Linux-Shipping" "$@"
