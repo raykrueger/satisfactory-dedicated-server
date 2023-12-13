ea20becbc8976e4189007b333f5579825641d128
Ray Krueger <raykrueger@gmail.com>
2023-12-13 16:20:10 +0000

    Disable Steam game update at launch by default
    Since I am doing a nightly build of the container, I can disable the
    update at launch by default.
    
    Running with STEAMUPDATE=true will update the game server inside the
    conatiner at startup.

--- start-server.sh
+++ start-server.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
 # Make sure we own the potential mount points
-chown -R $USERID:$USERID /home/steam/.config/Epic
+chown -R $USERID:$USERID /home/$USERNAME/.config/Epic
 
 gomplate --input-dir /tmp/config --output-dir /game/FactoryGame/Saved/Config/LinuxServer/
 chown -R ${USERID}:${USERID} /game/FactoryGame/Saved
 
-echo "Updating game server"
-gosu $USERID:$USERID steamcmd +force_install_dir /game +login anonymous +app_update 1690800 $STEAMARGS +quit
+if [ -n "$STEAMUPDATE" ] ; then
+	echo "Updating game server"
+	gosu $USERID:$USERID steamcmd +force_install_dir /game +login anonymous +app_update 1690800 $STEAMARGS +quit
+else
+	echo "Skipping game server update, set 'STEAMUPDATE=true' to enable"
+fi
 
 SERVERARGS="FactoryGame -NoSteamClient -unattended -log -Port=$SERVERGAMEPORT -BeaconPort=$SERVERBEACONPORT -ServerQueryPort=$SERVERQUERYPORT -multihome=0.0.0.0"
 
